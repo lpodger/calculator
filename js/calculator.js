4a9bd468515522d2ecaf89fc71ee80a8689b37e3
lpodger <114342815+lpodger@users.noreply.github.com>
2023-08-08 14:26:56 +0800

    Add and test basic functions

--- js/calculator.js
+++ js/calculator.js
@@ -1,14 +1,35 @@
 // Your calculator is going to contain functions for all of the basic math operators you typically find on simple calculators, so start by creating functions for the following items and testing them in your browser’s console.
 // add
+function add(a, b) {
+    return a + b;
+};
+
 // subtract
+function subtract(a, b) {
+    return a - b;
+};
+
 // multiply
+function multiply(a, b) {
+    return a * b;
+};
+
 // divide
-// A calculator operation will consist of a number, an operator, and another number. For example, 3 + 5. Create three variables for each of the parts of a calculator operation. Create a variable for the first number, the operator, and the second number. You’ll use these variables to update your display later.
+function divide(a ,b) {
+    return a / b;
+};
+
+let firstNumber = a;
+let secondNumber = b;
+let operator = x;
+
+
+// A calculator operation will consist of a number, an operator, and another number. 
+// For example, 3 + 5. Create three variables for each of the parts of a calculator operation. 
+// Create a variable for the first number, the operator, and the second number. You’ll use these variables to update your display later.
 // Create a new function operate that takes an operator and 2 numbers and then calls one of the above functions on the numbers.
-// Create a basic HTML calculator with buttons for each digit, each of the above functions and an “Equals” key.
-// Do not worry about wiring up the JS just yet.
-// There should also be a display for the calculator. Go ahead and fill it with some dummy numbers so it looks correct.
-// Add a “clear” button.
+
+
 // Create the functions that populate the display when you click the number buttons. You should be storing the ‘display value’ in a variable somewhere for use in the next step.
 // Make the calculator work! You’ll need to store the first number that is input into the calculator when a user presses an operator, and also save which operation has been chosen and then operate() on them when the user presses the “=” key.
 // You should already have the code that can populate the display, so once operate() has been called, update the display with the ‘solution’ to the operation.
