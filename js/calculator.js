5e76af41f98b026cda1b398dcea471292f256602
lpodger <114342815+lpodger@users.noreply.github.com>
2023-08-08 16:11:46 +0800

    Add display function

--- js/calculator.js
+++ js/calculator.js
@@ -45,7 +45,14 @@ function operate(operator, a, b) {
     }
 };
 
-// Create the functions that populate the display when you click the number buttons. You should be storing the ‘display value’ in a variable somewhere for use in the next step.
+// Create the functions that populate the display when you click the number buttons. 
+// You should be storing the ‘display value’ in a variable somewhere for use in the next step.
+
+function display() {
+    const display = document.getElementById("output");
+    display.textContent = displayContent;
+}
+
 // Make the calculator work! You’ll need to store the first number that is input into the calculator when a user presses an operator, and also save which operation has been chosen and then operate() on them when the user presses the “=” key.
 // You should already have the code that can populate the display, so once operate() has been called, update the display with the ‘solution’ to the operation.
 // This is the hardest part of the project. You need to figure out how to store all the values and call the operate function with them. Don’t feel bad if it takes you a while to figure out the logic.
